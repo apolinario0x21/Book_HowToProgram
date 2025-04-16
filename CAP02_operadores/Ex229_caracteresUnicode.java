6a4278fc61fc3571403d18a5f4dbf62024c5c3be
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-16 19:14:10 -0300

    add ex caracteres unicode

---
+++ CAP02_operadores/Ex229_caracteresUnicode.java
@@ -0,0 +1,22 @@
+package src.CAP02_operadores;
+
+public class Ex229_caracteresUnicode {
+    public static void main(String[] args) {
+        System.out.printf("Caracteres Unicode: %n %d %d %d %d %d %d %d %d %d %d %d %d %d",
+                (int) 'A',
+                (int) 'A',
+                (int) 'C',
+                (int) 'a',
+                (int) 'b',
+                (int) 'c',
+                (int) '0',
+                (int) '1',
+                (int) '2',
+                (int) '$',
+                (int) '*',
+                (int) '+',
+                (int) '/'
+        );
+
+    }
+}
