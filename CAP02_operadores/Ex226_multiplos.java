6195c8371ddf240f9015b5dbb61411537c8133aa
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-16 19:11:23 -0300

    add ex is multiplo

---
+++ CAP02_operadores/Ex226_multiplos.java
@@ -0,0 +1,10 @@
+package src.CAP02_operadores;
+
+public class Ex226_multiplos {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 20;
+
+        System.out.println(x % y == 0 ? "É multiplo" : "Não é multiplo");
+    }
+}
